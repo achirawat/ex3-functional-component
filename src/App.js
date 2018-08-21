d6459e3f1a3db727e82ebc495970ed739a9a36fa
Achirawat Juasawad <achirawat@angstromsolutions.net>
2018-08-21 15:40:58 +0700

    .

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import './App.css';
 import Game from './Game';
 import Score from './Score';
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
 const numQuestions = 0;
 const numCorrect = 0;
 
--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,49 @@
 import React, { Component } from 'react';
 
 class Game extends Component {
+    constructor(props) {
+        super(props);
+        const arrayValue = this.randomQuestion();
+        this.state = {
+            value1: arrayValue[0],
+            value2: arrayValue[1],
+            value3: arrayValue[2],
+            answer: arrayValue[3]
+        }
+    }
+
+    randomQuestion = () => {
+        const value1 = Math.floor(Math.random() * 100);
+        const value2 = Math.floor(Math.random() * 100);
+        const value3 = Math.floor(Math.random() * 100);
+        const answer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
+        return (value1, value2, value3, answer);
+    };
+
+    updateState = () => {
+        this.setSSate(currState => ({
+            value1: arrayValue[0],
+            value2: arrayValue[1],
+            value3: arrayValue[2],
+            answer: arrayValue[3]
+        }));
+    };
+
+    handleAnswer = event => {
+        const newArrayValue = this.randomQuestion();
+        this.setState(newArrayValue);
+    }
+
     render() {
-        // return();
+        return(
+            <div>
+                <div className="equation">
+                    <p className="text">{`${value1} + ${value2} + ${value3} = ${answer}`}</p>
+                </div>
+                <button  >True</button>
+                <button>False</button>
+            </div>
+        );
     }
 }
 
